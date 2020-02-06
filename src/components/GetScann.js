7ac6744c4ba4517f3e35919eace9d099bad50813
kalush81 <krzysiek.kaluza@gmail.com>
2020-02-06 16:56:28 +0100

    test JSON.parse

--- src/components/GetScann.js
+++ src/components/GetScann.js
@@ -1,44 +1,48 @@
-import React, { Component } from 'react'
-import QrReader from 'react-qr-scanner'
- 
+import React, { Component } from "react";
+import QrReader from "react-qr-scanner";
+
 export default class GetScan extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       delay: 100,
-      result: 'No result',
-    }
- 
+      result: "No result"
+    };
+
     //this.handleScan = this.handleScan.bind(this)
   }
-  handleScan = (data) =>{
+  handleScan = data => {
     this.setState({
-      result: data,
-    })
-  }
-  handleError = (err) => {
-    console.error(err)
-  }
-  render(){
+      result: JSON.parse(data)
+    });
+  };
+  handleError = err => {
+    console.error(err);
+  };
+  render() {
     const previewStyle = {
-      margin: 'auto auto',  
-      height: '330px',
+      margin: "auto auto",
+      height: "330px",
       width: "330px",
-      objectFit: 'cover'
-    }
- 
-    return(
+      objectFit: "cover"
+    };
+
+    return (
       <div>
         <QrReader
           legacyMode={false}
-          facingMode='rear'
+          facingMode="rear"
           delay={this.state.delay}
           style={previewStyle}
           onError={this.handleError}
           onScan={this.handleScan}
-          />
-        <p>{this.state.result}</p>
+        />
+        <p>
+          {this.state.result.name}
+          {this.state.result.lastName}
+          {this.state.result.certificate}
+        </p>
       </div>
-    )
+    );
   }
 }
